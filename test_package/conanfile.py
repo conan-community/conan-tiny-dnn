e2e86866ffaa1641dd3926ac3bcc491a7d458994
danimtb <danimanzaneque@gmail.com>
2018-03-15 16:05:50 +0100

    removed configure cppstd

--- test_package/conanfile.py
+++ test_package/conanfile.py
@@ -6,13 +6,6 @@ class TestPackageConan(ConanFile):
     settings = "os", "compiler", "build_type", "arch", "cppstd"
     generators = "cmake"
 
-    def configure(self):
-        if not self.settings.cppstd:
-            if self.settings.os == "Windows":
-                self.settings.cppstd = 14
-            else:
-                self.settings.cppstd = "gnu14"
-
     def build(self):
         cmake = CMake(self)
         cmake.configure()
