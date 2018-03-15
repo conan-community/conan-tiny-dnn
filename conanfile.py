f2a5d3042a0581b814b71d3dadc1259d9e054947
danimtb <danimanzaneque@gmail.com>
2018-03-15 12:35:12 +0100

    updated files

--- conanfile.py
+++ conanfile.py
@@ -1,4 +1,4 @@
-from conans import ConanFile, CMake, tools
+from conans import ConanFile, tools
 import os
 
 
@@ -6,17 +6,17 @@ class TinyDnnConan(ConanFile):
     name = "tiny-dnn"
     version = "0.1.1"
     license = "The BSD 3-Clause License"
-    homepage = "http://tiny-dnn.readthedocs.io/en/latest/"
-    url = "https://github.com/tiny-dnn/tiny-dnn"
-    settings = "os"
     description = ("tiny-dnn is a C++14 implementation of deep learning. "
                    "It is suitable for deep learning on limited computational resource, embedded "
                    "systems and IoT devices.")
+    homepage = "https://github.com/tiny-dnn/tiny-dnn"
+    url = "https://github.com/conan-community/conan-tiny-dnn"
+    settings = "os"
     no_copy_source = True
     exports_sources = ["LICENSE"]
 
     def source(self):
-        source_url = ("%s/archive/v%s.zip" % (self.url, self.version))
+        source_url = ("%s/archive/v%s.zip" % (self.homepage, self.version))
         tools.get(source_url)
         os.rename("%s-%s" % (self.name, self.version), "sources")
 
--- test_package/conanfile.py
+++ test_package/conanfile.py
@@ -1,18 +1,28 @@
-from conans import ConanFile, CMake
+from conans import ConanFile, CMake, tools
 import os
-import shutil
 
 
 class TestPackageConan(ConanFile):
-    settings = "os", "compiler", "build_type", "arch"
+    settings = "os", "compiler", "build_type", "arch", "cppstd"
     generators = "cmake"
 
+    def configure(self):
+        if not self.settings.cppstd:
+            if self.settings.os == "Windows":
+                self.settings.cppstd = 14
+            else:
+                self.settings.cppstd = "gnu14"
 
     def build(self):
         cmake = CMake(self)
         cmake.configure()
         cmake.build()
 
+    def imports(self):
+        self.copy(pattern="*.dll", src="bin", dst="bin")
+        self.copy(pattern="*.dylib", src="lib", dst="bin")
+
     def test(self):
-        bin_path = os.path.join("bin", "test_package")
-        self.run("%s %s" % (bin_path, os.path.abspath(os.path.join(os.path.dirname(__file__), "data"))))
+        with tools.chdir("bin"):
+            arg = os.path.abspath(os.path.join(os.path.dirname(__file__), "data"))
+            self.run(".%stest_package %s" % (os.sep, arg))
